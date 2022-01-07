85a25f10c0a09a1e51e9e25963d87ff83f27caca
niloba <57182409+niloba@users.noreply.github.com>
2022-01-07 09:11:49 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,14 +1,13 @@
 function checkTemp(temp){
-    if(temp<20){
+    if(temp<=20){
     console.log("liiga külm")
         return -1}
-    else if(temp>20 && temp<40){
+    else if(temp>20 && temp<=40){
     console.log("paras")
         return 0}
-    else if(temp>40 && temp<60){
+    else if(temp>=41 && temp<=60){
     console.log("liiga kuum")
         return 1}
     
 }
 checkTemp(prompt("Sisesta arv 1-60"))
-
