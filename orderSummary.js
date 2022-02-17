4833bdc50d1854ebcdfb3c4853127520fb109e50
niloba <57182409+niloba@users.noreply.github.com>
2022-02-17 15:33:15 +0200

    Create orderSummary.js

---
+++ orderSummary.js
@@ -0,0 +1,50 @@
+var tellimus = { 
+  "id": 2113, 
+  "orderNumber": "E210126001", 
+  "createdAt": "2021-01-26 16:04:24", 
+  "clientName": "Kalle Tali", 
+  "deliveryType": "Omniva smartpost", 
+  "deliveryValue": 178, 
+  "rows": [ 
+    { 
+      "article_id": 31, 
+      "name": "Kohvimasin De'Longhi Dolce Gusto Piccolo EDG100.W", 
+      "product_code": "257549", 
+      "price": 49.1666666667, 
+      "amount": 1, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23420, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Espresso Intenso, 128 g, 16 tk", 
+      "product_code": "355836", 
+      "price": 5.0666666667, 
+      "amount": 2, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23421, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Latte Macchiato, 194 g, 16 tk", 
+      "product_code": "000000000501267301", 
+      "price": 5.825, 
+      "amount": 1, 
+      "vat": 0.2 
+    } 
+  ] 
+} 
+function orderSummary(){
+    for(let i = 0; i < tellimus.rows.length; i++){
+        summ = ((tellimus.rows[i].price.toFixed(2) * tellimus.rows[i].amount))
+        console.log("Toode:" + tellimus.rows[i].name)
+        console.log("Kogus:" + tellimus.rows[i].amount)
+        console.log("Hind:" + tellimus.rows[i].price.toFixed(2) + "EUR")
+        console.log("Summa:" + summ + "EUR")
+    }
+}
+
+let kogusumma = 0
+  for (let i = 0; i < tellimus.rows.length; i++) {
+    kogusumma+= ((1 + tellimus.rows[i].vat) * (tellimus.rows[i].price * tellimus.rows[i].amount))
+    }    
+orderSummary()
+console.log("Kogusumma käibemaksuga:" , kogusumma.toFixed(2) + "EUR")
